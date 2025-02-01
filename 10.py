5aeaa069efb5e47c9764a14bf99e1716a480398e
affan50pro <sweet.baby12325@gmail.com>
2025-02-01 14:01:54 +0400

    first commit

---
+++ 10.py
@@ -0,0 +1,11 @@
+empty=[]
+print(empty)
+
+list=[1,2,3,4,5]
+print(list)
+
+multipy=[1,2,3*3]
+print(multipy)
+
+data=[200,300,400,"affan"]
+print(data)
---
+++ 102.py
@@ -0,0 +1,10 @@
+list=[1,2,3,4,5]
+print(list[0])
+
+
+list1=[1,2,3,4,5]
+print(list[0:4])
+
+list2=[1,2,3,4,5]
+list2.append(8)
+print(list2)
